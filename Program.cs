81adc02fcaf24402fb3b09f3f676410edb9d3669
Abubakar Saleh Usman <abubakarsalehusman@gmail.com>
2018-08-01 20:25:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using myCodeLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scrape mypage = new Scrape();
+
+           string value = mypage.ScrapeWebpage("http://msdn.microsoft.com");
+
+            Console.WriteLine(value);
+            
+        }
+    }
+}
